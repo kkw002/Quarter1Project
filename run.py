14e5b3ad1084d25335f49f10bf1dd4cf7f084abb
kkw002 <91997503+kkw002@users.noreply.github.com>
2022-12-03 22:41:39 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,22 @@
+import sys
+import json
+import pandas as pd
+sys.path.insert(0, 'src')
+from etl import getData
+from features import clean_features
+from train_model import rfModel
 
+def main(targets):
+    data_config = json.load(open('config/data-params.json'))
+    if 'test' in targets:
+      data = getData('test/testdata.pkl')
+      X,y = clean_features(data)
+      rfModel(X,y)
+    if 'results' in targets:
+      
+return 
+if __name__ == '__main__':
+    # run via:
+    # python main.py data features model
+    targets = sys.argv[1:]
+    main(targets)
