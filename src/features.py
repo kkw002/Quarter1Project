4936a574ff68bdc4ed296995dd6c5d0d9c305301
kkw002 <91997503+kkw002@users.noreply.github.com>
2022-12-03 16:46:45 -0800

    Update features.py

--- src/features.py
+++ src/features.py
@@ -1,1 +1,10 @@
-
+import pandas as pd
+def clean_features(df):
+  encoded_X= pd.get_dummies(df,columns=['acquisition_type','snapshot_type','channel'])
+  encoded_X['vantage3_score'].fillna(encoded_X['vantage3_score'].mean(), inplace=True)
+  #removing these columns specifically
+  X=encoded_X.loc[:, ~encoded_X.columns.isin(['bad','bad_balance','vintage','state_code','bad_v2','evaluation_dt'])]
+  y = encoded_X['bad']
+  X=X.replace(np.NaN,-999)
+  y=y.replace(np.NaN,-2)
+  return X,y
