ff92ec5fd1dc22912203b5ef17fd4b4ecbc6e923
kkw002 <91997503+kkw002@users.noreply.github.com>
2022-12-03 16:51:26 -0800

    Update and rename model.py to train_model.py

---
+++ src/train_model.py
@@ -0,0 +1,8 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+def rfmodel(X,y):
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
+  hyperparam_model=RandomForestClassifier(n_estimators=48,min_samples_split=2,min_samples_leaf=2,max_features='auto',max_depth=2,bootstrap=True)
+  hyperparam_model.fit(X_train,y_train)
+  return metrics.roc_auc_score(y_test.to_numpy(),hyperparam_model.predict_proba(X_test),multi_class='ovr',average='weighted')
