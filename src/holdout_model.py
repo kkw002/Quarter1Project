d072c79d4c028f3a972e775c72e545a5fddf6a22
kkw002 <91997503+kkw002@users.noreply.github.com>
2022-12-05 11:56:10 -0800

    Update holdout_model.py

--- src/holdout_model.py
+++ src/holdout_model.py
@@ -1,6 +1,4 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 from sklearn import metrics
 def holdout_results(rmodel,xmodel,X,y):
-  print('Holdout Random Forest AUC: '+metrics.roc_auc_score(y.to_numpy(),rmodel.predict_proba(X),multi_class='ovr',average='weighted'))
-  print('Holdout XGB AUC: '+metrics.roc_auc_score(y.to_numpy(),xmodel.predict_proba(X),multi_class='ovr',average='weighted'))
+  print('Holdout Random Forest AUC: '+str(metrics.roc_auc_score(y.to_numpy(),rmodel.predict_proba(X)[:,1])))
+  print('Holdout XGB AUC: '+str(metrics.roc_auc_score(y.to_numpy(),xmodel.predict_proba(X)[:,1])))
