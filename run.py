c487428f5253a0db1f6a77ee200a08b61537ced0
kkw002 <91997503+kkw002@users.noreply.github.com>
2022-12-04 22:29:36 -0800

    Update run.py

--- run.py
+++ run.py
@@ -2,27 +2,23 @@ import sys
 import json
 import pandas as pd
 sys.path.insert(0, 'src')
-from etl import getData
+from etl import getData,getDataZip
 from features import clean_features
-from train_model import rfModel
+from train_model import model_gen
 from holdout_model import holdout_results
 
 def main(targets):
     if 'test' in targets:
         data = getData('data/testdata.pkl')
         X,y = clean_features(data)
-        rfModel(X,y)
-    if 'train' in targets:
-        data = getData('data/forStudents.pkl.zip)
-        X,y = clean_features(data)
-        rfModel(X,y)
-    if 'holdout' in targets:
+        model_gen(X,y)
+    if 'results' in targets:
         train_set = getData('data/forStudents.pkl.zip)
         holdout_set = getData('data/holdout_final.pkl.zip)
         train_X,train_y = clean_features(train_set)
         holdout_X,holdout_y = clean_features(holdout_set)
-        model = rfModel(train_X,train_y)
-        holdout_results(model,holdout_X,holdout_y)
+        randmodel,xgbmodel = model_gen(train_X,train_y)
+        holdout_results(randmodel,xgbmodel,holdout_X,holdout_y)
 return 
 if __name__ == '__main__':
     # run via:
