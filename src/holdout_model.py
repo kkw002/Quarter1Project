54a3bf31e40b127cfd7a5731e4aa8b65f4c56aa1
kkw002 <91997503+kkw002@users.noreply.github.com>
2022-12-05 16:47:35 -0800

    Update holdout_model.py

--- src/holdout_model.py
+++ src/holdout_model.py
@@ -1,4 +1,22 @@
 from sklearn import metrics
+import matplotlib.pyplot as plt
+import numpy as np
 def holdout_results(rmodel,xmodel,X,y):
   print('Holdout Random Forest AUC: '+str(metrics.roc_auc_score(y.to_numpy(),rmodel.predict_proba(X)[:,1])))
+  print('Generating Plot...\n')
+  fpr,tpr,threshholds = metrics.roc_curve(np.array(y_test),rmodel.predict_proba(X_test)[:,1])
+  plt.plot(fpr,tpr,label='AUC= '+str(metrics.roc_auc_score(y_test.to_numpy(),rmodel.predict_proba(X_test)[:,1])))
+  plt.ylabel('True Positive Rate')
+  plt.xlabel('False Positive Rate')
+  plt.legend()
+  plt.savefig('auc_plots/holdoutrf_auc_plot')
+  plt.clf()
   print('Holdout XGB AUC: '+str(metrics.roc_auc_score(y.to_numpy(),xmodel.predict_proba(X)[:,1])))
+  print('Generating Plot...\n')
+  fpr,tpr,threshholds = metrics.roc_curve(np.array(y_test),xmodel.predict_proba(X_test)[:,1])
+  plt.plot(fpr,tpr,label='AUC= '+str(metrics.roc_auc_score(y_test.to_numpy(),xmodel.predict_proba(X_test)[:,1])))
+  plt.ylabel('True Positive Rate')
+  plt.xlabel('False Positive Rate')
+  plt.legend()
+  plt.savefig('auc_plots/holdoutxgb_auc_plot')
+  plt.clf()
