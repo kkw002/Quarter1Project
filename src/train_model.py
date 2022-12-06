ac79f84218dff396c246a01d0cb491d774ad66bf
kkw002 <91997503+kkw002@users.noreply.github.com>
2022-12-05 16:47:19 -0800

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from xgboost import XGBClassifier
+import matplotlib.pyplot as plt
+import numpy as np
 
 def model_gen(X,y):
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=42)
@@ -25,19 +27,43 @@ def model_gen(X,y):
 
   print('Baseline Test AUC: '+str(metrics.roc_auc_score(y_test.to_numpy(),lr_model.predict_proba(X_test)[:,1]))+'\n')
     
+  fpr,tpr,threshholds = metrics.roc_curve(np.array(y_test),lr_model.predict_proba(X_test)[:,1])
+  
+  plt.plot(fpr,tpr,label='AUC= '+str(metrics.roc_auc_score(y_test.to_numpy(),lr_model.predict_proba(X_test)[:,1])))
+  plt.ylabel('True Positive Rate')
+  plt.xlabel('False Positive Rate')
+  plt.legend()
+  plt.savefig('auc_plots/baseline_auc_plot')
+  plt.clf()
   #Display AUC for Random Forest
 
   print('Training Data Random Forest AUC:  '+str(metrics.roc_auc_score(y_train.to_numpy(),rf_model.predict_proba(X_train)[:,1])))
 
   print('Test Data Random Forest AUC: '+str(metrics.roc_auc_score(y_test.to_numpy(),rf_model.predict_proba(X_test)[:,1]))+'\n')
-        
+  
+  print('Generating plot...\n')
+  fpr,tpr,threshholds = metrics.roc_curve(np.array(y_test),rf_model.predict_proba(X_test)[:,1])
+  plt.plot(fpr,tpr,label='AUC= '+str(metrics.roc_auc_score(y_test.to_numpy(),rf_model.predict_proba(X_test)[:,1])))
+  plt.ylabel('True Positive Rate')
+  plt.xlabel('False Positive Rate')
+  plt.legend()
+  plt.savefig('auc_plots/rf_auc_plot')
+  plt.clf()
+
   #Displaying the AUC For XGB
 
   print('Training Data XGB AUC:  '+str(metrics.roc_auc_score(y_train.to_numpy(),xgb_model.predict_proba(X_train)[:,1])))
-    
   print('Test Data XGB AUC: '+str(metrics.roc_auc_score(y_test.to_numpy(),xgb_model.predict_proba(X_test)[:,1]))+'\n')
-  
-  #optimized model for Random Forest 
+  print('Generating plot...\n')
+  fpr,tpr,threshholds = metrics.roc_curve(np.array(y_test),xgb_model.predict_proba(X_test)[:,1])
+  plt.plot(fpr,tpr,label='AUC= '+str(metrics.roc_auc_score(y_test.to_numpy(),xgb_model.predict_proba(X_test)[:,1])))
+  plt.ylabel('True Positive Rate')
+  plt.xlabel('False Positive Rate')
+  plt.legend()
+  plt.savefig('auc_plots/xgb_auc_plot')
+  plt.clf()
+
+  #optimized model for Random Forest
   #param_grid = {'n_estimators': [int(x) for x in np.linspace(start = 10, stop = 80, num = 10)],
   #             'max_features': ['auto', 'sqrt'],
   #             'max_depth': [2,4],
@@ -51,6 +77,14 @@ def model_gen(X,y):
   hyperparam_model.fit(X_train,y_train)
   print('Training Data Optimized RandomForest AUC:  '+str(metrics.roc_auc_score(y_train.to_numpy(),hyperparam_model.predict_proba(X_train)[:,1])))
   print('Test Data Optimized RandomForest AUC:  '+str(metrics.roc_auc_score(y_test.to_numpy(),hyperparam_model.predict_proba(X_test)[:,1]))+'\n')
+  print('Generating plot...\n')
+  fpr,tpr,threshholds = metrics.roc_curve(np.array(y_test),hyperparam_model.predict_proba(X_test)[:,1])
+  plt.plot(fpr,tpr,label='AUC= '+str(metrics.roc_auc_score(y_test.to_numpy(),hyperparam_model.predict_proba(X_test)[:,1])))
+  plt.ylabel('True Positive Rate')
+  plt.xlabel('False Positive Rate')
+  plt.legend()
+  plt.savefig('auc_plots/optimizedrf_auc_plot')
+  plt.clf()
   #optimized model for XGBoost
   #params = {
   #  'learning_rate':[0.05,0.1,0.15,0.20,0.25],
@@ -68,5 +102,12 @@ def model_gen(X,y):
 
   print('Training Data Optimized XGB AUC: '+str(metrics.roc_auc_score(y_train.to_numpy(),hxgb_model.predict_proba(X_train)[:,1])))
   print('Test Data Optimized XGB AUC: '+str(metrics.roc_auc_score(y_test.to_numpy(),hxgb_model.predict_proba(X_test)[:,1]))+'\n')
-        
+  print('Generating plot...\n')
+  fpr,tpr,threshholds = metrics.roc_curve(np.array(y_test),hxgb_model.predict_proba(X_test)[:,1])
+  plt.plot(fpr,tpr,label='AUC= '+str(metrics.roc_auc_score(y_test.to_numpy(),hxgb_model.predict_proba(X_test)[:,1])))
+  plt.ylabel('True Positive Rate')
+  plt.xlabel('False Positive Rate')
+  plt.legend()
+  plt.savefig('auc_plots/optimizedxgb_auc_plot')
+  plt.clf()
   return hyperparam_model,hxgb_model
